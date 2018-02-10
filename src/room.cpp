bfa4330b3f75a92dbe9c31f9096056a27f9323d0
Neil Connatty <neil.connatty@gmail.com>
2018-02-09 16:00:17 -0800

    change formatting

--- src/room.cpp
+++ src/room.cpp
@@ -4,134 +4,122 @@
 
 #define SPRITE_SIZE 64.f
 
-Room::Room()
-{
-}
+Room::Room() {}
 
-Room::~Room()
-{
-}
+Room::~Room() {}
 
 // Creates a room
-bool Room::init(vec2 position)
+bool Room::init(vec2 position) 
 {
-	m_position = position;
-	m_scale = { 2.f, 2.f };
+  m_position = position;
+  m_scale = {2.f, 2.f};
 
-	return true;
+  return true;
 }
 
-bool Room::init()
-{
-	return init({ 0.f, 0.f });
-}
+bool Room::init() { return init({0.f, 0.f}); }
 
 // Releases all associated resources
-void Room::destroy()
+void Room::destroy() 
 {
-	for (Floor f : m_floors)
-	{
-		f.destroy();
-	}
+  for (Floor f : m_floors) 
+  {
+    f.destroy();
+  }
 
-	for (Wall w : m_walls)
-	{
-		w.destroy();
-	}
+  for (Wall w : m_walls) 
+  {
+    w.destroy();
+  }
 }
 
-void Room::update_current(float ms)
-{
-}
+void Room::update_current(float ms) {}
 
 void Room::update_children(float ms)
 // at the moment the room's only children are walls and floors,
 // which do not need to be updated
 {}
 
-void Room::draw_current(const mat3& projection, const mat3& current_transform)
+void Room::draw_current(const mat3 &projection, const mat3 &current_transform) 
 {
 }
 
 // Renders our room
-void Room::draw_children(const mat3& projection, const mat3& current_transform)
+void Room::draw_children(const mat3 &projection,
+                         const mat3 &current_transform) 
 {
 
-	for (Floor f : m_floors)
-	{
-		f.draw(projection, current_transform);
-	}
+  for (Floor f : m_floors) 
+  {
+    f.draw(projection, current_transform);
+  }
 
-	for (Wall w : m_walls)
-	{
-		w.draw(projection, current_transform);
-	}
-	
-	for (Puddle p : m_puddles)
-	{
-		p.draw(projection, current_transform);
-	}
+  for (Wall w : m_walls) 
+  {
+    w.draw(projection, current_transform);
+  }
+
+  for (Puddle p : m_puddles) 
+  {
+    p.draw(projection, current_transform);
+  }
 }
 
-bool Room::add_wall(wall_pair wall)
+bool Room::add_wall(wall_pair wall) 
 {
   m_walls.emplace_back();
-  if (!m_walls.back().init(wall.first, wall.second))
+  if (!m_walls.back().init(wall.first, wall.second)) 
   {
     return false;
   }
 }
 
-bool Room::add_floor(vec2 floor)
+bool Room::add_floor(vec2 floor) 
 {
   m_floors.emplace_back();
-  if (!m_floors.back().init(floor))
+  if (!m_floors.back().init(floor)) 
   {
     return false;
   }
 }
 
-bool Room::add_floors(std::vector<vec2>& positions)
+bool Room::add_floors(std::vector<vec2> &positions) 
 {
-	for (vec2& pos : positions)
-	{
-    if (!add_floor(pos))
+  for (vec2 &pos : positions) 
+  {
+    if (!add_floor(pos)) 
     {
       return false;
     }
-	}
+  }
 
-	return true;
+  return true;
 }
 
-bool Room::add_walls(std::vector<wall_pair>& walls)
+bool Room::add_walls(std::vector<wall_pair> &walls) 
 {
-	for (wall_pair& pair : walls)
-	{
-    if (!add_wall(pair))
+  for (wall_pair &pair : walls) 
+  {
+    if (!add_wall(pair)) 
     {
       return false;
     }
-	}
+  }
 
-	return true;
+  return true;
 }
 
-bool Room::add_cleanables(std::vector<vec2>& puddle_positions)
+bool Room::add_cleanables(std::vector<vec2> &puddle_positions) 
 {
-	for (vec2& pos : puddle_positions)
-	{
-		m_puddles.emplace_back();
-		if (!m_puddles.back().init(pos))
-
-		{
-			return false;
-		}
-	}
-	return true;
+  for (vec2 &pos : puddle_positions) 
+  {
+    m_puddles.emplace_back();
+    if (!m_puddles.back().init(pos))
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
-std::vector<Puddle>& Room::get_cleanables()
-{
-	return m_puddles;
-}
+std::vector<Puddle> &Room::get_cleanables() { return m_puddles; }
--- src/roomtextparser.cpp
+++ src/roomtextparser.cpp
@@ -10,82 +10,68 @@
 #define FLOOR 'f'
 #define PUDDLE 'p'
 
-bool RoomParser::parseLine(std::string& line, float y, bool first_line)
-{
-	float x = 0.f;
+bool RoomParser::parseLine(std::string &line, float y, bool first_line) {
+  float x = 0.f;
   size_t i = 0;
   wall_edge edge = NONE;
   vec2 tile_dim;
 
-	for (; i < line.size(); ++i)
-	{
-    const char& ch = line[i];
+  for (; i < line.size(); ++i) {
+    const char &ch = line[i];
 
-    if (first_line)
-    {
+    if (first_line) {
       edge = TOP;
     }
-    if (i == 0 || i == line.size()-1)
-    {
+    if (i == 0 || i == line.size() - 1) {
       edge = (wall_edge)(edge | VERTICAL);
     }
 
-		if (ch == SPACE)
-		{
-			continue;
-		}
-		else if (ch == WALL)
-		{
-      wall_pairs.push_back({ {x, y}, edge });
+    if (ch == SPACE) {
+      continue;
+    } else if (ch == WALL) {
+      wall_pairs.push_back({{x, y}, edge});
       tile_dim = Wall::get_dimensions(edge);
       x = x + tile_dim.x;
-		}
-		else if (ch == FLOOR)
-		{
-			floor_pos.push_back({ x, y });
+    } else if (ch == FLOOR) {
+      floor_pos.push_back({x, y});
       tile_dim = Floor::get_dimensions();
       x = x + tile_dim.x;
-		}
-		else if (ch == PUDDLE)
-		{
-			puddle_pos.push_back({ x, y });
-			floor_pos.push_back({ x, y });
+    } else if (ch == PUDDLE) {
+      puddle_pos.push_back({x, y});
+      floor_pos.push_back({x, y});
       tile_dim = Floor::get_dimensions();
       x = x + tile_dim.x;
-		}
-		else
-		{
-			fprintf(stderr, "Error parsing room file. Invalid character %c at line %d, column %d.\n", ch, (int)y + 1, (int)x + 1);
-			return false;
-		}
-	}
+    } else {
+      fprintf(stderr,
+              "Error parsing room file. Invalid character %c at line %d, "
+              "column %d.\n",
+              ch, (int)y + 1, (int)x + 1);
+      return false;
+    }
+  }
 
-	return true;
+  return true;
 }
 
-bool RoomParser::parseRoom(Room& room, const char* filename)
-{
-	std::string line;
-	std::ifstream file(filename);
+bool RoomParser::parseRoom(Room &room, const char *filename) {
+  std::string line;
+  std::ifstream file(filename);
 
-	wall_pairs.clear();
-	floor_pos.clear();
+  wall_pairs.clear();
+  floor_pos.clear();
   puddle_pos.clear();
 
-	float y = 0.f;
+  float y = 0.f;
   bool first_line = true;
-  while (std::getline(file, line))
-  {
-    if (!parseLine(line, y, first_line))
-    {
+  while (std::getline(file, line)) {
+    if (!parseLine(line, y, first_line)) {
       fprintf(stderr, "Error parsing room file: %s.\n", filename);
       return false;
     }
     y = y + 25.f;
-    
 
-    if (!(room.add_floors(floor_pos) && room.add_walls(wall_pairs) && room.add_cleanables(puddle_pos)))
-    {
+    if (!(room.add_floors(floor_pos) && room.add_walls(wall_pairs) &&
+          room.add_cleanables(puddle_pos))) {
       fprintf(stderr, "Issue parsing room file: %s.\n", filename);
       return false;
     }
@@ -94,5 +80,5 @@ bool RoomParser::parseRoom(Room& room, const char* filename)
     puddle_pos.clear();
   }
 
-	return true;
+  return true;
 }
