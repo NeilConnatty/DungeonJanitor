b15cc818c5935eb498d31db5e45188a55362f787
Neil Connatty <neil.connatty@gmail.com>
2018-03-07 11:02:46 -0800

    remove linear texture magnification filter

--- src/wall.cpp
+++ src/wall.cpp
@@ -211,6 +211,7 @@ void Wall::draw_current(const mat3& projection, const mat3& current_transform)
 	// Enabling and binding texture to slot 0
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, m_texture->id);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	// Setting uniform values to the currently bound program
 	glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float*)&current_transform);
