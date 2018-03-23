19978a43b7f7825ea1760f2e48a2d3cb16760566
sgdon <sgdonachiue@gmail.com>
2018-03-23 14:29:55 -0700

    The janitor is invisible, my headache isn't.

--- src/janitor.cpp
+++ src/janitor.cpp
@@ -8,14 +8,7 @@
 #include <cmath>
 
 //This is ugly af
-Texture Janitor::up1; Texture Janitor::up2; Texture Janitor::up3; Texture Janitor::up4;
-Texture Janitor::up_right1; Texture Janitor::up_right2; Texture Janitor::up_right3; Texture Janitor::up_right4;
-Texture Janitor::up_left1; Texture Janitor::up_left2; Texture Janitor::up_left3; Texture Janitor::up_left4;
-Texture Janitor::down1; Texture Janitor::down2; Texture Janitor::down3; Texture Janitor::down4;
-Texture Janitor::down_right1; Texture Janitor::down_right2; Texture Janitor::down_right3; Texture Janitor::down_right4;
-Texture Janitor::down_left1; Texture Janitor::down_left2; Texture Janitor::down_left3; Texture Janitor::down_left4;
-Texture Janitor::left1; Texture Janitor::left2; Texture Janitor::left3; Texture Janitor::left4;
-Texture Janitor::right1; Texture Janitor::right2; Texture Janitor::right3; Texture Janitor::right4;
+Texture Janitor::janitor_sheet;
 
 Janitor::Janitor() : GameObject()
 {
@@ -29,19 +22,20 @@ bool Janitor::init(vec2 position)
 {
 	if (!validate_textures()) return false;
 	//scale of the texture and resulting mesh
-	//all the player textures are the same size so it's fine to pick one arbitrarily
-	float wr = up2.width * 0.5f;
-	float hr = up2.height * 0.5f;
-
+	float wr = janitor_sheet.width/4 * 0.5f;
+	float hr = janitor_sheet.height/8 * 0.5f;
+	animation_frame_w = 1 / 4;
+	animation_frame_h = 1 / 8;
+	//texcoords are in the range [0..1]
 	TexturedVertex vertices[4];
 	vertices[0].position = { -wr, +hr, -0.02f };
-	vertices[0].texcoord = { 0.f, 1.f };
+	vertices[0].texcoord = { 0.f, 0.f };
 	vertices[1].position = { +wr, +hr, -0.02f };
-	vertices[1].texcoord = { 1.f, 1.f };
+	vertices[1].texcoord = { animation_frame_w, 0 };
 	vertices[2].position = { +wr, -hr, -0.02f };
-	vertices[2].texcoord = { 1.f, 0.f };
+	vertices[2].texcoord = { animation_frame_w, animation_frame_h };
 	vertices[3].position = { -wr, -hr, -0.02f };
-	vertices[3].texcoord = { 0.f, 0.f };
+	vertices[3].texcoord = { 0.f, animation_frame_h };
 
 	// counterclockwise as it's the default opengl front winding direction
 	uint16_t indices[] = { 0, 3, 1, 1, 3, 2 };
@@ -64,13 +58,15 @@ bool Janitor::init(vec2 position)
 		return false;
 
 	// Loading shaders
-	if (!effect.load_from_file(shader_path("textured.vs.glsl"), shader_path("textured.fs.glsl")))
+	if (!effect.load_from_file(shader_path("animated.vs.glsl"), shader_path("animated.fs.glsl")))
 		return false;
 
 	//Initialize member variables
 	m_position = position;
-	m_size = {static_cast<float>(up2.width), static_cast<float>(up2.height)};
-	m_animation_time = 0;
+	m_size = {static_cast<float>(janitor_sheet.width/4), static_cast<float>(janitor_sheet.height/8)};
+	animation_dir = down;
+	frame = 1;
+	m_time_elapsed = 0;
 	m_time_pressed_up = 0;
 	m_time_pressed_down = 0;
 	m_time_pressed_left = 0;
@@ -80,7 +76,6 @@ bool Janitor::init(vec2 position)
 	m_key_left = false;
 	m_key_right = false;
 	m_door_collision_last_frame = false;
-	m_curr_tex = &up1;
 	return true;
 }
 
@@ -120,16 +115,11 @@ void Janitor::update_current(float ms)
 	//const float Y_SPEED = SPEED * (24.f / 35.f);
 	float time_factor = ms / 1000;
 
-	const int NUM_FRAMES = 4;	//4 frames of animation per direction
-	const int FRAME_TIMING = 80; //80 ms per frame of animation (12.5fps)
+	m_time_elapsed += ms;
+	if (m_time_elapsed > MS_PER_FRAME)
+		frame = (frame + 1) % NUM_FRAMES;
 
-
-	if (m_animation_time > FRAME_TIMING * NUM_FRAMES)
-		m_animation_time = 0;
-	else
-		m_animation_time += ms;
-  
-  check_collisions();
+	check_collisions();
 	//UP
 	if (m_key_up)
 	{
@@ -264,7 +254,7 @@ void Janitor::update_current(float ms)
 	m_position.x = m_position.x + m_vel.x * time_factor;
 	m_position.y = m_position.y + m_vel.y * time_factor;
   
-	pick_movement_tex(FRAME_TIMING);
+	pick_movement_tex();
 }
 
 void Janitor::check_collisions()
@@ -302,6 +292,12 @@ void Janitor::draw_current(const mat3& projection, const mat3& current_transform
 	GLint transform_uloc = glGetUniformLocation(effect.program, "transform");
 	GLint color_uloc = glGetUniformLocation(effect.program, "fcolor");
 	GLint projection_uloc = glGetUniformLocation(effect.program, "projection");
+	//animation uniforms
+	GLint frame_uloc = glGetUniformLocation(effect.program, "frame");
+	GLint animation_dir_uloc = glGetUniformLocation(effect.program, "animation_dir");
+	GLfloat animation_frame_w_uloc = glGetUniformLocation(effect.program, "animation_frame_w");
+	GLfloat animation_frame_h_uloc = glGetUniformLocation(effect.program, "animation_frame_h");
+
 
 	// Setting vertices and indices
 	glBindVertexArray(mesh.vao);
@@ -321,16 +317,19 @@ void Janitor::draw_current(const mat3& projection, const mat3& current_transform
 	
 	// Enabling and binding texture to slot 0
 	glActiveTexture(GL_TEXTURE0);
-	//dereferencing a null pointer ic ic.
-	glBindTexture(GL_TEXTURE_2D, m_curr_tex->id);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glBindTexture(GL_TEXTURE_2D, janitor_sheet.id);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	// Setting uniform values to the currently bound program
 	glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float*)&current_transform);
 	float color[] = { 1.f, 1.f, 1.f };
 	glUniform3fv(color_uloc, 1, color);
 	glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float*)&projection);
-
+	//animation uniforms
+	glUniform1iv(frame_uloc, 1, &frame);
+	glUniform1iv(animation_dir_uloc, 1, (int*)&animation_dir);
+	glUniform1fv(animation_frame_w_uloc, 1, &animation_frame_w);
+	glUniform1fv(animation_frame_h_uloc, 1, &animation_frame_h);
 	// Drawing!
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
 }
@@ -455,7 +454,7 @@ void Janitor::key_right(bool move) {
 
 //Helper function for draw_current
 //sets *m_curr_tex = &some_tex where some_tex is based on timing and key(s) pressed
-void Janitor::pick_movement_tex(const int FRAME_TIMING) {
+void Janitor::pick_movement_tex() {
 	
 	//if velocity is 0, return, leaving the current animation frame as is
 	float x_vel = abs(m_vel.x);
@@ -464,14 +463,14 @@ void Janitor::pick_movement_tex(const int FRAME_TIMING) {
 	if (x_vel > y_vel)
 		vel_magnitude = x_vel;
 	if (vel_magnitude == 0) return;
-	
+
 	//Pick current texture based on direction of velocity
 	vec2 vel_dir = normalize(m_vel);
 	vec2 default_dir = { 1, 0 };
 	float theta = acos(dot(vel_dir, default_dir)); //gives the angle of our velocity (but only from 0-pi in rads)
 	if (vel_dir.y > 0) theta = -theta;	//flip negative values for the bottom half of the unit circle
 	float pi = atan(1) * 4;
-	DIRECTION animation_dir = right;
+	animation_dir = right;
 	if (theta < 3 * pi / 8 && theta > pi / 8)
 		animation_dir = up_right;
 	else if (theta < 5 * pi / 8 && theta > 3 * pi / 8)
@@ -490,361 +489,15 @@ void Janitor::pick_movement_tex(const int FRAME_TIMING) {
 	//if (theta < pi / 8 || theta > -pi / 8)
 	else
 		animation_dir = right;
-	switch (animation_dir)
-	{
-		case up:
-		{
-			if (m_animation_time < FRAME_TIMING)	
-				m_curr_tex = &up1;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &up2;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &up3;
-			else if (m_animation_time >= 3 * FRAME_TIMING)
-				m_curr_tex = &up4;
-			break;
-		}
-		case up_right:
-		{
-			if (m_animation_time < FRAME_TIMING)						
-				m_curr_tex = &up_right1;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &up_right2;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &up_right3;
-			else if (m_animation_time >= 3 * FRAME_TIMING)				
-				m_curr_tex = &up_right4;
-			break;
-		}
-		case up_left:
-		{
-			if (m_animation_time < FRAME_TIMING)
-				m_curr_tex = &up_left4;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)
-				m_curr_tex = &up_left3;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)
-				m_curr_tex = &up_left2;
-			else if (m_animation_time >= 3 * FRAME_TIMING)	
-				m_curr_tex = &up_left1;
-			break;
-		}
-		case down:
-		{
-			if (m_animation_time < FRAME_TIMING)
-				m_curr_tex = &down1;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)
-				m_curr_tex = &down2;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)
-				m_curr_tex = &down3;
-			else if (m_animation_time >= 3 * FRAME_TIMING)
-				m_curr_tex = &down4;
-			break;
-		}
-		case down_right:
-		{
-			if (m_animation_time < FRAME_TIMING)	
-				m_curr_tex = &down_right4;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &down_right3;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &down_right2;
-			else if (m_animation_time >= 3 * FRAME_TIMING)
-				m_curr_tex = &down_right1;
-			break;
-		}
-		case down_left:
-		{
-			if (m_animation_time < FRAME_TIMING)	
-				m_curr_tex = &down_left1;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &down_left2;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &down_left3;
-			else if (m_animation_time >= 3 * FRAME_TIMING)				
-				m_curr_tex = &down_left4;
-			break;
-		}
-		case right:
-		{
-			if (m_animation_time < FRAME_TIMING)	
-				m_curr_tex = &right4;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &right3;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &right2;
-			else if (m_animation_time >= 3 * FRAME_TIMING)
-				m_curr_tex = &right1;
-			break;
-		}
-		case left:
-		{
-			if (m_animation_time < FRAME_TIMING)	
-				m_curr_tex = &left1;
-			else if (m_animation_time >= FRAME_TIMING && m_animation_time < 2 * FRAME_TIMING)	
-				m_curr_tex = &left2;
-			else if (m_animation_time >= 2 * FRAME_TIMING && m_animation_time < 3 * FRAME_TIMING)	
-				m_curr_tex = &left3;
-			else if (m_animation_time >= 3 * FRAME_TIMING) 
-				m_curr_tex = &left4;
-		}
-	}
 }
 //Loads and validates texture files via hardcoded path names
 bool Janitor::validate_textures()
 {
-	if (!up1.is_valid())
+	if (!janitor_sheet.is_valid())
 	{
-		if (!up1.load_from_file(textures_path("janitor/up1.png")))
+		if (!janitor_sheet.load_from_file(textures_path("janitor/janitorsheet.png")))
 		{
-			fprintf(stderr, "Failed to load texture up1\n");
-			return false;
-		}
-	}
-	if (!up2.is_valid())
-	{
-		if (!up2.load_from_file(textures_path("janitor/up2.png")))
-		{
-			fprintf(stderr, "Failed to load texture up2\n");
-			return false;
-		}
-	}
-	if (!up3.is_valid())
-	{
-		if (!up3.load_from_file(textures_path("janitor/up3.png")))
-		{
-			fprintf(stderr, "Failed to load texture up3\n");
-			return false;
-		}
-	}
-	if (!up4.is_valid())
-	{
-		if (!up4.load_from_file(textures_path("janitor/up4.png")))
-		{
-			fprintf(stderr, "Failed to load texture up4\n");
-			return false;
-		}
-	}
-	if (!down1.is_valid())
-	{
-		if (!down1.load_from_file(textures_path("janitor/down1.png")))
-		{
-			fprintf(stderr, "Failed to load texture down1\n");
-			return false;
-		}
-	}
-	if (!down2.is_valid())
-	{
-		if (!down2.load_from_file(textures_path("janitor/down2.png")))
-		{
-			fprintf(stderr, "Failed to load texture down2\n");
-			return false;
-		}
-	}
-	if (!down3.is_valid())
-	{
-		if (!down3.load_from_file(textures_path("janitor/down3.png")))
-		{
-			fprintf(stderr, "Failed to load texture down3\n");
-			return false;
-		}
-	}
-	if (!down4.is_valid())
-	{
-		if (!down4.load_from_file(textures_path("janitor/down4.png")))
-		{
-			fprintf(stderr, "Failed to load texture down4\n");
-			return false;
-		}
-	}
-	if (!left1.is_valid())
-	{
-		if (!left1.load_from_file(textures_path("janitor/left1.png")))
-		{
-			fprintf(stderr, "Failed to load texture left1\n");
-			return false;
-		}
-	}
-	if (!left2.is_valid())
-	{
-		if (!left2.load_from_file(textures_path("janitor/left2.png")))
-		{
-			fprintf(stderr, "Failed to load texture left2\n");
-			return false;
-		}
-	}
-	if (!left3.is_valid())
-	{
-		if (!left3.load_from_file(textures_path("janitor/left3.png")))
-		{
-			fprintf(stderr, "Failed to load texture left3\n");
-			return false;
-		}
-	}
-	if (!left4.is_valid())
-	{
-		if (!left4.load_from_file(textures_path("janitor/left4.png")))
-		{
-			fprintf(stderr, "Failed to load texture left4\n");
-			return false;
-		}
-	}
-	if (!right1.is_valid())
-	{
-		if (!right1.load_from_file(textures_path("janitor/right1.png")))
-		{
-			fprintf(stderr, "Failed to load texture right1\n");
-			return false;
-		}
-	}
-	if (!right2.is_valid())
-	{
-		if (!right2.load_from_file(textures_path("janitor/right2.png")))
-		{
-			fprintf(stderr, "Failed to load texture right2\n");
-			return false;
-		}
-	}
-	if (!right3.is_valid())
-	{
-		if (!right3.load_from_file(textures_path("janitor/right3.png")))
-		{
-			fprintf(stderr, "Failed to load texture right3\n");
-			return false;
-		}
-	}
-	if (!right4.is_valid())
-	{
-		if (!right4.load_from_file(textures_path("janitor/right4.png")))
-		{
-			fprintf(stderr, "Failed to load texture right4\n");
-			return false;
-		}
-	}
-	if (!up_right1.is_valid())
-	{
-		if (!up_right1.load_from_file(textures_path("janitor/up_right1.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_right1\n");
-			return false;
-		}
-	}
-	if (!up_right2.is_valid())
-	{
-		if (!up_right2.load_from_file(textures_path("janitor/up_right2.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_right2\n");
-			return false;
-		}
-	}
-	if (!up_right3.is_valid())
-	{
-		if (!up_right3.load_from_file(textures_path("janitor/up_right3.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_right3\n");
-			return false;
-		}
-	}
-	if (!up_right4.is_valid())
-	{
-		if (!up_right4.load_from_file(textures_path("janitor/up_right4.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_right4\n");
-			return false;
-		}
-	}
-	if (!up_left1.is_valid())
-	{
-		if (!up_left1.load_from_file(textures_path("janitor/up_left1.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_left1\n");
-			return false;
-		}
-	}
-	if (!up_left2.is_valid())
-	{
-		if (!up_left2.load_from_file(textures_path("janitor/up_left2.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_left2\n");
-			return false;
-		}
-	}
-	if (!up_left3.is_valid())
-	{
-		if (!up_left3.load_from_file(textures_path("janitor/up_left3.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_left3\n");
-			return false;
-		}
-	}
-	if (!up_left4.is_valid())
-	{
-		if (!up_left4.load_from_file(textures_path("janitor/up_left4.png")))
-		{
-			fprintf(stderr, "Failed to load texture up_left4\n");
-			return false;
-		}
-	}
-	if (!down_right1.is_valid())
-	{
-		if (!down_right1.load_from_file(textures_path("janitor/down_right1.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_right1\n");
-			return false;
-		}
-	}
-	if (!down_right2.is_valid())
-	{
-		if (!down_right2.load_from_file(textures_path("janitor/down_right2.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_right2\n");
-			return false;
-		}
-	}
-	if (!down_right3.is_valid())
-	{
-		if (!down_right3.load_from_file(textures_path("janitor/down_right3.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_right3\n");
-			return false;
-		}
-	}
-	if (!down_right4.is_valid())
-	{
-		if (!down_right4.load_from_file(textures_path("janitor/down_right4.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_right4\n");
-			return false;
-		}
-	}
-	if (!down_left1.is_valid())
-	{
-		if (!down_left1.load_from_file(textures_path("janitor/down_left1.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_left1\n");
-			return false;
-		}
-	}
-	if (!down_left2.is_valid())
-	{
-		if (!down_left2.load_from_file(textures_path("janitor/down_left2.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_left2\n");
-			return false;
-		}
-	}
-	if (!down_left3.is_valid())
-	{
-		if (!down_left3.load_from_file(textures_path("janitor/down_left3.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_left3\n");
-			return false;
-		}
-	}
-	if (!down_left4.is_valid())
-	{
-		if (!down_left4.load_from_file(textures_path("janitor/down_left4.png")))
-		{
-			fprintf(stderr, "Failed to load texture down_left4\n");
+			fprintf(stderr, "Failed to load janitor spritesheet\n");
 			return false;
 		}
 	}
