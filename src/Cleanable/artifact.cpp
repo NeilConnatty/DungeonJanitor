ecf74b4f47677f4073713ed3f0164b3b5039ae53
Dana Gregg <danagregg@hotmail.com>
2018-04-14 19:24:08 -0700

    Fixed crash from cherrypick and reset values to default

--- src/Cleanable/artifact.cpp
+++ src/Cleanable/artifact.cpp
@@ -37,8 +37,8 @@ bool Artifact::init(vec2 position)
 	position.y = position.y + 27;
 	m_position = position;
 	m_size = {static_cast<float>(artifact_textures[0].width), static_cast<float>(artifact_textures[0].height)};
-	m_is_dirty = false;
-	m_is_activated = true;
+	m_is_dirty = true;
+	m_is_activated = false;
 	m_sound = Mix_LoadWAV(audio_path("artifact.wav"));
 
 	// The position corresponds to the center of the texture
