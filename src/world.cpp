b89c5cc26f6f79814d96b7efe111e1209f151279
Sean Donachiue <sgdonachiue@gmail.com>
2018-04-14 18:45:32 -0700

    Update world.cpp

--- src/world.cpp
+++ src/world.cpp
@@ -73,7 +73,6 @@ bool World::init(vec2 screen)
 
     //-------------------------------------------------------------------------
     // Loading music and sounds
-	/*
     if (SDL_Init(SDL_INIT_AUDIO) < 0)
     {
         fprintf(stderr, "Failed to initialize SDL Audio");
@@ -100,7 +99,7 @@ bool World::init(vec2 screen)
     Mix_PlayMusic(m_background_music, -1);
 
     fprintf(stderr, "Loaded music");
-	*/
+	
     //actually put something like return m_janitor.init();
 	if (!m_dungeon.init())
 	{
@@ -178,13 +177,13 @@ bool World::init_hero()
 // Releases all the associated resources
 void World::destroy()
 {
-	/*
+	
   if (m_background_music != nullptr)
     Mix_FreeMusic(m_background_music);
 
   Mix_CloseAudio();
   SDL_Quit();
-  */
+  
   m_dungeon.destroy();
   m_janitor.destroy();
   m_hero.destroy();
